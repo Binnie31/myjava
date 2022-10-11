a53433ff70498a3d7e729ba33fb5edc592b2e55a
binnie <binnie3105@gmail.com>
2022-10-11 00:06:31 -0700

    Commiting the day2 of Git

---
+++ src/com/day2/Trail2.java
@@ -0,0 +1,8 @@
+package com.day2;
+
+public class Trail2 {
+	public static void main(String[] args) {
+		System.out.println("This is trial for day2g");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Binamra.Thapa
+ *
+ */
+module Project2 {
+}
